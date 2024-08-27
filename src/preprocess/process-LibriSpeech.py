33f228cd7abe04008c4e95c548074be198fa5d4d
sadPororo <65598585+sadPororo@users.noreply.github.com>
2024-08-27 18:12:42 +0900

    Add files via upload

---
+++ src/preprocess/process-LibriSpeech.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+import itertools
+import subprocess
+
+from tqdm import tqdm as tqdm
+from multiprocessing import Pool, freeze_support
+
+from os.path import join as opj
+
+
+parser = argparse.ArgumentParser(description = "LibriSpeech preprocessing")
+parser.add_argument('--read_path', type=str, default="./data/LibriSpeech", help='Source directory')
+parser.add_argument('--save_path', type=str, default=None, help='Output directory, default to "{READ_PATH}/preprocess/"')
+parser.add_argument('--n_workers', type=int, default=4, help='Multi-processing Cores')
+args = parser.parse_args();
+
+
+def _generator(iterable):
+    for i in iterable: yield i
+    
+def preprocess_conversion(filepath_list):
+    local_path, read_path, save_path = filepath_list
+    
+    out = subprocess.call('ffmpeg -i %s %s -c:a pcm_s16le -y >/dev/null 2>&1'%(opj(read_path, local_path), opj(save_path, local_path.replace('.flac', '.wav'))), shell=True)
+    # out = subprocess.call('ffmpeg -i %s %s -y >/dev/null 2>&1'%(opj(read_path, local_path), opj(save_path, local_path.replace('.flac', '.wav'))), shell=True)
+    if out != 0:
+        raise ValueError('Conversion failed %s.'% opj(read_path, local_path))
+    
+
+if __name__ == "__main__":
+    freeze_support()
+
+    # Define output directory
+    if args.save_path is None:
+        save_path = opj(args.read_path, 'preprocess')
+    else:
+        save_path = args.save_path
+    os.makedirs(opj(save_path, 'wav16'), exist_ok=True)
+    os.makedirs(opj(save_path, 'speakers'), exist_ok=True)
+    out = subprocess.call('cp %s %s' % (opj(args.read_path, 'SPEAKERS.TXT'), opj(save_path, 'speakers', 'SPEAKERS.TXT')), shell=True)
+    if out != 0:
+        raise ValueError('Copy failed %s.' % opj(args.read_path, 'SPEAKERS.TXT'))
+    os.makedirs(opj(save_path, 'trials'), exist_ok=True)
+        
+    subset_list = ['dev-clean', 'dev-other', 'test-clean', 'test-other', 'train-clean-100', 'train-clean-360', 'train-other-500']
+    for subset in subset_list:
+        print('Processing on Subset: %s' % subset)
+        
+        # gather total file list to preprocess
+        print('Gathering Source (Speaker/Chapter/Audio) paths...')
+        total_filepath_list = []
+        for speaker_id in _generator(tqdm(os.listdir(opj(args.read_path, subset)))):
+            
+            for chapter_id in _generator(os.listdir(opj(args.read_path, subset, speaker_id))):
+                os.makedirs(opj(save_path, 'wav16', subset, speaker_id, chapter_id), exist_ok=True)
+                
+                for wav_id in _generator(os.listdir(opj(args.read_path, subset, speaker_id, chapter_id))):
+                    if wav_id.endswith('.flac'):
+                        total_filepath_list.append(opj(subset, speaker_id, chapter_id, wav_id))
+                        
+        # resample to 16K
+        print('Converting (.flac) to (.wav) files...')    
+        with Pool(args.n_workers) as pool:
+            list(tqdm(pool.imap(preprocess_conversion, zip(total_filepath_list, 
+                                                           itertools.repeat(opj(args.read_path)), 
+                                                           itertools.repeat(opj(save_path, 'wav16')))), total=len(total_filepath_list)))
---
+++ src/preprocess/split-VCTK-0-speakers.py
@@ -0,0 +1,103 @@
+import os
+import re
+import argparse
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from itertools import product
+from os.path import join as opj
+from tqdm import tqdm as tqdm
+
+
+parser = argparse.ArgumentParser(description = "VCTK-Corpus set split")
+parser.add_argument('--data_path', type=str, default="./data/VCTK-Corpus/preprocess", help='Source directory')
+args = parser.parse_args();
+
+
+def _generator(iterable):
+    for i in iterable: yield i
+
+def remove_whitespace(line:str):
+    line = line[line.find(' '):]
+    while line[0] == ' ':
+        line = line[1:]            
+    return line
+
+def binning_columns(column: pd.Series, n_bins: int):
+    bin_labels = pd.cut(column, n_bins, retbins=True)[-1]
+    bin_labels = ((bin_labels[:-1] + bin_labels[1:]) // 2).astype(int)
+    return pd.cut(column, n_bins, labels=bin_labels)
+    
+if __name__ == "__main__":
+    
+    # load speaker meta info
+    with open(opj(args.data_path, 'speakers', 'speaker-info.txt'), 'r') as f:
+        speaker_meta = []
+        for line in _generator(f.readlines()[1:]):
+            id  = line[:line.find(' ')].strip(); line = remove_whitespace(line)
+            age = line[:line.find(' ')].strip(); line = remove_whitespace(line)
+            gender  = line[:line.find(' ')].strip(); line = remove_whitespace(line)
+            accents = line[:line.find(' ')].strip(); line = remove_whitespace(line)
+            region  = re.sub(' +', ' ', line[:line.find('\n')].strip())
+            
+            speaker_meta.append(['p'+id, age, gender, accents, region])
+
+    speaker_meta = pd.DataFrame(speaker_meta, columns=['ID', 'AGE', 'GENDER', 'ACCENTS', 'REGION']).set_index('ID')
+    
+    # exclude speaker [p280, p315] of risen technical issues
+    speaker_list = speaker_meta.index.to_list()
+    if 'p280' in speaker_list:
+        speaker_list.remove('p280')
+    if 'p315' in speaker_list:
+        speaker_list.remove('p315')
+    speaker_meta = speaker_meta.loc[speaker_list]
+    
+    # collect utterance counts for speakers
+    sample_counts_per_speaker = {}
+    for speaker_id in tqdm(speaker_list):
+        sample_counts_per_speaker[speaker_id] = len(os.listdir(opj(args.data_path, 'wav16', speaker_id)))
+        
+    speaker_meta['COUNTS'] = pd.Series(sample_counts_per_speaker)
+    
+    # binning too much specified columns [AGE, COUNTS]
+    speaker_meta['AGE'] = speaker_meta['AGE'].astype(int)
+    speaker_meta['AGE2'] = binning_columns(speaker_meta['AGE'], 2)
+    speaker_meta['AGE3'] = binning_columns(speaker_meta['AGE'], 3)
+    
+    speaker_meta['COUNTS2'] = binning_columns(speaker_meta['COUNTS'], 2)
+    speaker_meta['COUNTS3'] = binning_columns(speaker_meta['COUNTS'], 3)
+
+    # Stratified Split (Train | Val-test)
+    stratify_conditions = ['AGE3', 'GENDER', 'ACCENTS', 'COUNTS2']
+
+    indices = speaker_meta[stratify_conditions].value_counts().index[
+        np.where(speaker_meta[stratify_conditions].value_counts()==1)[0]]
+    
+    single_instances = np.zeros_like(np.array(speaker_meta.index)).astype(bool)
+    for i in range(len(indices)):
+        single_instances |= np.array(speaker_meta[stratify_conditions]==indices[i]).all(axis=1)
+
+    df_train, df_val_test = train_test_split(speaker_meta[~single_instances], test_size=0.4, random_state=42, stratify=speaker_meta[~single_instances][stratify_conditions])
+    df_train = pd.concat([df_train, speaker_meta[single_instances]])
+    
+    # Stratified split (valid | test)
+    indices = df_val_test[stratify_conditions].value_counts().index[
+        np.where(df_val_test[stratify_conditions].value_counts()==1)[0]]
+    single_instances = np.zeros_like(np.array(df_val_test.index)).astype(bool)
+
+    for i in range(len(indices)):
+        single_instances |= np.array(df_val_test[stratify_conditions]==indices[i]).all(axis=1)
+
+    df_val, df_test = train_test_split(df_val_test[~single_instances], test_size=0.5, random_state=42, stratify=df_val_test[~single_instances][stratify_conditions])
+    df_val_test = shuffle(df_val_test[single_instances], random_state=42)
+    df_val  = pd.concat([df_val, df_val_test[:len(df_val_test)//2]])
+    df_test = pd.concat([df_test, df_val_test[len(df_val_test)//2:]])
+    
+    # save csv
+    save_columns = list(set(['AGE', 'GENDER', 'ACCENTS', 'REGION', 'COUNTS'] + stratify_conditions))
+    df_train[save_columns].to_csv(opj(args.data_path, 'speakers', 'speaker-train.csv'))
+    df_val[save_columns].to_csv(opj(args.data_path, 'speakers', 'speaker-valid.csv'))
+    df_test[save_columns].to_csv(opj(args.data_path, 'speakers', 'speaker-test.csv'))
